b2b29abd354fc1ff3b58ade8ae2163a189fb1db9
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 17:22:29 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,20 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  return_val = []
   while i < collection.count
-    yield(collection[i])
+    return_val << yield(collection[i])
     i += 1 
   end
+  
+  if
+    return_val.include?(false)
+      false
+    else
+      true 
+  end
+end
+
+my_all?([1,2,3]) do |i|
+  i < 2
 end
